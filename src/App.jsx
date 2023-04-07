76638c2508cbf8f54bc9006f3c9d14fd86ee7218
Samuel Soyebo <ayodejisoyebo@gmail.com>
2023-04-07 16:10:00 +0100

    Medical Profile Initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Map from "./components/Map";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
 
 
 function App() {
   return (
     <>
-      <Map />
+      {/* <Map />
       <Signup />
-      <Login />
+      <Login /> */}
+      <UserProfile/>
     </>
   );
   
