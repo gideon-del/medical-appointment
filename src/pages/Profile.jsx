f74b14566c6ae8e06db2abdbfd27f736920e0f35
Judge-Paul <judgepaulogebe3@gmail.com>
2023-04-21 07:37:27 +0100

    fix: final profile touchup

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { BiUserCircle, BiBody } from "react-icons/bi";
 import { AiTwotoneStar } from "react-icons/ai";
 import { BsCalendarMinusFill, BsGenderAmbiguous } from "react-icons/bs";
-import { FiEdit2 } from "react-icons/fi";
+import { FaEdit } from "react-icons/fa";
 import { Link, Navigate, useLocation } from "react-router-dom";
 import { MdBloodtype } from "react-icons/md";
 import { GiBodyHeight } from "react-icons/gi";
@@ -126,61 +126,72 @@ const Profile = () => {
         </main> */}
         <main className="bg-[#E2E8F0] pt-5 pt-5 md:pt-10 lg:pt-14 px-5 md:px-14 lg:px-20 font-workSans">
           <div className="flex flex-col lg:flex-row gap-10">
-            <div className="w-full lg:w-2/3 md:flex bg-white rounded-2xl p-5 lg:p-8">
-              <div className="w-full md:w-2/5 text-center">
-                <img src="https://leadership.ng/wp-content/uploads/2023/03/davido.png" alt="Profile Icon" className="w-24 h-24 rounded-full mx-auto" />
-                <h4 className="text-2xl font-bold text-black truncate mt-2">{profile?.name}</h4>
-                <p className="text-gray-500 mt-2 truncate">{profile?.email}</p>
-                <h4 className="text-lg text-black font-bold mt-4 font-poppins">Appointments</h4>
-                <h4 className="text-5xl font-bold text-black mx-auto px-6 w-max border border-t-black">{appointments?.length}</h4>
-                <p className="text-gray-400">Upcoming</p>
+            <div className="w-full lg:w-2/3 bg-white rounded-2xl p-5 lg:p-8">
+              <div className="flex justify-between">
+                <h4 className="text-black text-2xl font-bold text-black">Payments</h4>
+                <Link to="/edit">
+                  <div className="flex bg-gray-300 hover:bg-gray-400 my-auto px-6 py-2 rounded-2xl font-semibold gap-2">
+                    <FaEdit className="my-auto" />
+                    Edit Profile
+                  </div>
+                </Link>
               </div>
-              <div className="grid grid-cols-2 gap-5 px-6 w-full md:w-3/5 mt-5 md:mt-0 md:border-l md:border-gray-500">
-                <div>
-                  <h4 className="text-lg md:text-xl text-gray-500">Gender</h4>
-                  <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.gender}</p>
-                  <hr />
+              <div className="md:flex mt-5">
+                <div className="w-full md:w-2/5 text-center">
+                  <img src="https://leadership.ng/wp-content/uploads/2023/03/davido.png" alt="Profile Icon" className="w-24 h-24 rounded-full mx-auto" />
+                  <h4 className="text-2xl font-bold text-black truncate mt-2">{profile?.name}</h4>
+                  <p className="text-gray-500 mt-2 truncate">{profile?.email}</p>
+                  <h4 className="text-lg text-black font-bold mt-4 font-poppins">Appointments</h4>
+                  <h4 className="text-5xl font-bold text-black mx-auto px-6 w-max border border-t-black">{appointments?.length}</h4>
+                  <p className="text-gray-400">Upcoming</p>
                 </div>
-                <div>
-                  <h4 className="text-lg md:text-xl text-gray-500">Birthday</h4>
-                  <p className="mt-1 mb-1.5 font-semibold text-black truncate">{convertUnixTimestamp(profile?.dateOfBirth.seconds)}</p>
-                  <hr />
-                </div>
-                <div>
-                  <h4 className="text-lg md:text-xl text-gray-500">Phone Number</h4>
-                  <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.phoneNumber}</p>
-                  <hr />
-                </div>
-                <div>
-                  <h4 className="text-lg md:text-xl text-gray-500">Marital Status</h4>
-                  <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.maritalStatus}</p>
-                  <hr />
-                </div>
-                <div>
-                  <h4 className="text-lg md:text-xl text-gray-500">Blood Group</h4>
-                  <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.bloodGroup}</p>
-                  <hr />
-                </div>
-                <div>
-                  <h4 className="text-lg md:text-xl text-gray-500">State</h4>
-                  <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.address}</p>
-                  <hr />
-                </div>
-                <div>
-                  <h4 className="text-lg md:text-xl text-gray-500">Height</h4>
-                  <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.heigth} cm</p>
-                  <hr />
-                </div>
-                <div>
-                  <h4 className="text-lg md:text-xl text-gray-500">Weight</h4>
-                  <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.weigth} kg</p>
-                  <hr />
+                <div className="grid grid-cols-2 gap-5 px-6 w-full md:w-3/5 mt-5 md:mt-0 md:border-l md:border-gray-500">
+                  <div>
+                    <h4 className="text-lg md:text-xl text-gray-500">Gender</h4>
+                    <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.gender}</p>
+                    <hr />
+                  </div>
+                  <div>
+                    <h4 className="text-lg md:text-xl text-gray-500">Birthday</h4>
+                    <p className="mt-1 mb-1.5 font-semibold text-black truncate">{convertUnixTimestamp(profile?.dateOfBirth.seconds)}</p>
+                    <hr />
+                  </div>
+                  <div>
+                    <h4 className="text-lg md:text-xl text-gray-500">Phone Number</h4>
+                    <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.phoneNumber}</p>
+                    <hr />
+                  </div>
+                  <div>
+                    <h4 className="text-lg md:text-xl text-gray-500">Marital Status</h4>
+                    <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.maritalStatus}</p>
+                    <hr />
+                  </div>
+                  <div>
+                    <h4 className="text-lg md:text-xl text-gray-500">Blood Group</h4>
+                    <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.bloodGroup}</p>
+                    <hr />
+                  </div>
+                  <div>
+                    <h4 className="text-lg md:text-xl text-gray-500">State</h4>
+                    <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.address}</p>
+                    <hr />
+                  </div>
+                  <div>
+                    <h4 className="text-lg md:text-xl text-gray-500">Height</h4>
+                    <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.heigth} cm</p>
+                    <hr />
+                  </div>
+                  <div>
+                    <h4 className="text-lg md:text-xl text-gray-500">Weight</h4>
+                    <p className="mt-1 mb-1.5 font-semibold text-black truncate">{profile?.weigth} kg</p>
+                    <hr />
+                  </div>
                 </div>
               </div>
             </div>
             <div className="w-full lg:w-1/3 bg-white rounded-2xl p-5">
               <h4 className="text-black text-2xl font-bold text-black">Payments</h4>
-              <div className="flex items-center justify-center text-black h-60 text-3xl font-bold">
+              <div className="flex items-center justify-center text-black h-72 text-3xl font-bold">
                 Coming Soon
               </div>
             </div>
@@ -196,7 +207,7 @@ const Profile = () => {
                 </button>
               </div>
               <Link to="/book-appointment">
-                <button className="w-full mt-5 md:mt-0 md:w-max basis-1 bg-blue-500 px-5 py-3 text-white font-semibold rounded-2xl">
+                <button className="w-full mt-5 md:mt-0 md:w-max basis-1 bg-blue-500 hover:bg-blue-700 px-5 py-3 text-white font-semibold rounded-2xl">
                   Make Appointment
                 </button>
               </Link>
